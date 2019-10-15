8bf56c1f84fc8525dcf28608c81de7cce79efd0e
deepthi_it19 <deepthi.it19@bitsathy.ac.in>
2019-10-15 16:00:30 +0530

    distbytrain

---
+++ prog1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    int n=80+40;
+    printf("\nlength of the train and bridge is 40m and 80m respectively\n");
+    printf("distance to be travelled by the train is %dm",n);
+    return 0;
+}
+
