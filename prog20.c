0a84b66b8c5d43b45c48ca648edae888090dfcc6
deepthi_it19 <deepthi.it19@bitsathy.ac.in>
2019-11-09 14:13:52 +0530

    oddeven

---
+++ prog20.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("enter the numer :");
+    scanf("%d",&n);
+    if(n%2==0)
+    printf("the given nr %d is even",n);
+    else
+    printf("the given nr %d is odd",n);
+}
